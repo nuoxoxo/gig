9c4f6d4c9131df7c177ebb764ed89b01c004e9c0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-04 20:31:42 +0200

    predeployment : vi vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {'process.env': {}}
+  base: '/',
+    build: {
+      assetsInclude: ['src/App.css'],
+    }
+  }
+
+  if (command !== 'serve') {
+    config.base = '/gig/'
+  }
+
+  return config
 })
