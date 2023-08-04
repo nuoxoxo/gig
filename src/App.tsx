0df6da048347866032311a432343be5d4a748209
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-04 22:13:16 +0200

    predeployment : trying button

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,16 @@ var App = () => {
             <li>
               {/* <button onClick={() => window.location.href="/route2210"}>
               22:10</button> */}
-              <Link to="/route2210">22:10</Link>
+              <button>
+                <Link to="/route2210">22:10</Link>
+              </button>
             </li>
             <li>
               {/* <button onClick={() => window.location.href="/route1802"}>
               18:02</button> */}
-              <Link to="/route1802">18:02</Link>
+              <button>
+                <Link to="/route1802">18:02</Link>
+              </button>
             </li>
             {/* <li>
               <button onClick={() => window.location.href="/route2113"}>
