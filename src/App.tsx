aeff1d71a5a9d880367dab4608faa46090486c44
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-05 17:04:28 +0200

    switch : no router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import { useState } from "react"
 import Route1802 from "./Routes/Route1802"
 import Route2210 from "./Routes/Route2210"
 
+type TargetRoute = React.FC
+
 var App = () => {
   const [ route, setRoute ] = useState<number>( 0 )
   const handleSetRoute = (n: number) => {
     setRoute(n)
   }
+  const routes: { [key: number]: TargetRoute } = {
+    2210: Route2210,
+    1802: Route1802,
+  }
+  const TargetRoute = routes[route]
   return (
     <>
       <nav>
@@ -19,8 +26,7 @@ var App = () => {
           </li>
         </ul>
       </nav>
-      { route === 2210 ? <Route2210 /> : null }
-      { route === 1802 ? <Route1802 /> : null }
+      { routes.hasOwnProperty(route) ? <TargetRoute /> : null }
     </>
   )
 }
