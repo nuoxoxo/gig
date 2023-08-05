35860361f472ab8da03151d178eb2ec1e440a572
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-05 19:30:49 +0200

    + gallery : a fcc project clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import { useState } from "react"
 import Route1802 from "./Routes/Route1802"
 import Route2210 from "./Routes/Route2210"
+import RouteCats from "./Routes/RouteCats"
 
 type TargetRoute = React.FC
 
 var App = () => {
-  const [ route, setRoute ] = useState<number>( 0 )
-  const handleSetRoute = (n: number) => {
-    setRoute(n)
+  const [ route, setRoute ] = useState<string>( '' )
+  const handleSetRoute = (s: string) => {
+    setRoute(s)
   }
-  const routes: { [key: number]: TargetRoute } = {
-    2210: Route2210,
-    1802: Route1802,
+  const routes: { [key: string]: TargetRoute } = {
+    '2210': Route2210,
+    '1802': Route1802,
+    'cats': RouteCats
   }
   const TargetRoute = routes[route]
   return (
     <>
       <nav>
         <ul>
-          <li>
-            <button onClick={ () => handleSetRoute(2210) }>22:10</button>
-          </li>
-          <li>
-            <button onClick={ () => handleSetRoute(1802) }>18:02</button>
-          </li>
+          <li><button onClick={()=>handleSetRoute('2210')}>22:10</button></li>
+          <li><button onClick={()=>handleSetRoute('1802')}>18:02</button></li>
+          <li><button onClick={()=>handleSetRoute('cats')}>chats</button></li>
         </ul>
       </nav>
       { routes.hasOwnProperty(route) ? <TargetRoute /> : null }
