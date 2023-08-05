1922dee5ea01b03ce86b2f203d22ec403a54c744
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-05 20:07:03 +0200

    + js block ---> cut repetitive code

--- src/Routes/RouteCats.tsx
+++ src/Routes/RouteCats.tsx
@@ -1,8 +1,19 @@
 var RouteCats = () => {
 
+  const L:string = 'https://cdn.freecodecamp.org/curriculum/css-photo-gallery/'
+  const Urls = Array.from(
+    { length: 9 }, (_, idx) => L + `${idx + 1}.jpg`
+  )
+
   return (
     <>
       <div className="gallery">
+
+        {Urls.map((url, idx) => (
+          <img key={idx} src={url} />
+        ))}
+
+        {/* 
         <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg" />
         <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/2.jpg" />
         <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/3.jpg" />
@@ -12,6 +23,8 @@ var RouteCats = () => {
         <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/7.jpg" />
         <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/8.jpg" />
         <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/9.jpg" />
+        */}
+
       </div>
     </>
   )
