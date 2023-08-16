479e74205c5769752c2c69d961e01ca3795956f2
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-17 00:22:15 +0200

    1802 : fixing, test edit

--- src/includes/Aoc1802.tsx
+++ src/includes/Aoc1802.tsx
@@ -1,18 +1,17 @@
 import { useState, useEffect } from "react"
 import { FetchData, LenNStrsFromLine } from "../helpers/Helpers"
 
-const path = 
-  "https://raw.githubusercontent.com/nuoxoxo/in/main/1802.in"
+const path = "https://raw.githubusercontent.com/nuoxoxo/in/main/1802.in"
 
-const special2 = '🔵'
-const special3 = '🟠'
-const fontSize1802 = '17px'
+const special2 = "🔵"
+const special3 = "🟠"
+const fontSize1802 = "17px"
 
 var Aoc1802 = () => {
-  const [ lines, setLines ] = useState<string[]>( [] )
-  const [ lines23, setLines23 ] = useState<string[]>( [] )
-  const [ p1, setP1 ] = useState<number>( 0 )
-  const [ p2, setP2 ] = useState<string>( '' )
+  const [lines, setLines] = useState<string[]>([])
+  const [lines23, setLines23] = useState<string[]>([])
+  const [p1, setP1] = useState<number>(0)
+  const [p2, setP2] = useState<string>("")
 
   const handleData = async () => {
     try {
@@ -24,7 +23,6 @@ var Aoc1802 = () => {
   }
 
   const Solver_Part_One = () => {
-
     let c2: number = 0
     let c3: number = 0
     let tempLines23: string[] = []
@@ -32,31 +30,31 @@ var Aoc1802 = () => {
       // console.log(line)
       let D: number[] = new Array(26).fill(0)
       for (const c of line) {
-        D[c.charCodeAt(0) - 'a'.charCodeAt(0)]++
+        D[c.charCodeAt(0) - "a".charCodeAt(0)]++
       }
       let ok2: boolean = false
       let ok3: boolean = false
-      let char2: string = ''
-      let char3: string = ''
+      let char2: string = ""
+      let char3: string = ""
       let strToPush: string = line
       let i: number = -1
       while (++i < D.length) {
         if (D[i] === 2) {
           ok2 = true
-          char2 = String.fromCharCode(i + 'a'.charCodeAt(0))
+          char2 = String.fromCharCode(i + "a".charCodeAt(0))
         }
         if (D[i] === 3) {
           ok3 = true
-          char3 = String.fromCharCode(i + 'a'.charCodeAt(0))
+          char3 = String.fromCharCode(i + "a".charCodeAt(0))
         }
       }
       if (ok2) {
         c2++
-        strToPush = strToPush.replace(new RegExp(char2, 'gi'), special2)
+        strToPush = strToPush.replace(new RegExp(char2, "gi"), special2)
       }
       if (ok3) {
         c3++
-        strToPush = strToPush.replace(new RegExp(char3, 'gi'), special3)
+        strToPush = strToPush.replace(new RegExp(char3, "gi"), special3)
       }
       if (strToPush.length !== 0) {
         tempLines23.push(strToPush)
@@ -67,27 +65,26 @@ var Aoc1802 = () => {
   }
 
   const Solver_Part_Two = () => {
-
     let i: number = -1
     let len: number = lines.length > 0 ? lines[0].length : 0
-    let res2: string = ''
-    while (++i < len)
-    {
+    let res2: string = ""
+    while (++i < len) {
       let j: number = -1
       while (++j < lines.length) {
-        let L: string = lines[j].substr(0, i) + lines[j].substr(i + 1, len - 1 - i)
+        let L: string =
+          lines[j].substr(0, i) + lines[j].substr(i + 1, len - 1 - i)
         let k: number = -1
         while (++k < lines.length) {
           if (j === k) {
             continue
           }
-          let R: string = lines[k].substr(0, i) + lines[k].substr(i + 1, len - 1 - i)
+          let R: string =
+            lines[k].substr(0, i) + lines[k].substr(i + 1, len - 1 - i)
           if (L === R) {
             res2 = L
             break
           }
         }
-
       }
     }
     setP2(res2)
@@ -104,39 +101,36 @@ var Aoc1802 = () => {
 
   return (
     <>
-    { lines ?
-      <div className='playground'>
-        <div className="field res"
-          style={{ fontSize: fontSize1802 }}
-        >
-          <span>--- 2018 Day 2: Inventory Management System ---</span>
-          <span>Part 1: {p1}</span>
-          <span>Part 2: {p2}</span>
-        </div>
-        <div className="field data-field"
-          style={{ fontSize: fontSize1802 }}
-        >
-          { lines
-            ? lines.length === 1
-              ? LenNStrsFromLine(lines[0], 16).join("\n")
-              : lines.join("\n")
-            : "No data available."
-          }
-        </div>
-        <div className="field data-field" 
-          style={{ fontSize: fontSize1802 }} 
-        >
-          { lines
-            ? lines.length === 1
-              ? LenNStrsFromLine(lines23[0], 16).join("\n")
-              : lines23.join("\n")
-            : "No data available."
-          }
-        </div>
-      </div>
-      :
-      <p>Loading data...</p>
-}
+      {lines ? (
+        <>
+          <div className="field res" style={{ fontSize: fontSize1802 }}>
+            <span>--- 2018 Day 2: Inventory Management System ---</span>
+            <span>Part 1: {p1}</span>
+            <span>Part 2: {p2}</span>
+          </div>
+          <div className="playground">
+            <div className="field data-field" style={{ fontSize: fontSize1802 }}>
+              {lines
+                ? lines.length === 1
+                  ? LenNStrsFromLine(lines23[0], 16).join("\n")
+                  : lines23.join("\n")
+                : "No data available."}
+            </div>
+            <div
+              className="field data-field"
+              style={{ fontSize: fontSize1802 }}
+            >
+              {lines
+                ? lines.length === 1
+                  ? LenNStrsFromLine(lines[0], 16).join("\n")
+                  : lines.join("\n")
+                : "No data available."}
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </>
   )
 }
