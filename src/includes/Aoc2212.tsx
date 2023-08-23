ab93f438b3e5333bf601a97cdc61bd3169432f92
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-24 00:18:59 +0200

    2212 : done draft, now uses bfs trace path

--- src/includes/Aoc2212.tsx
+++ src/includes/Aoc2212.tsx
@@ -1,22 +1,26 @@
 import { useState, useEffect } from "react"
 import { FetchData, LenNStrsFromLine } from "../helpers/Helpers"
 
-const path = "https://raw.githubusercontent.com/nuoxoxo/in/main/2212.in"
-const symbolArr = ['○']//, '·', '•', '▓']
+const URL:string = "https://raw.githubusercontent.com/nuoxoxo/in/main/2212.in"
+
+const symbolArr = ["○"] //, '·', '•', '▓']
 const symbol = symbolArr[Math.floor(Math.random() * symbolArr.length)]
+const denseSymbol = '○'
+const density:number = 170
 
 var Aoc2212 = () => {
   const [lines, setLines] = useState<string[]>([])
+
   const [p1, setP1] = useState<number>(0)
   const [p1Grid, setP1Grid] = useState<string[]>([])
+
   const [p2, setP2] = useState<number>(0)
   const [p2Grid, setP2Grid] = useState<string[]>([])
-  // const [p2Trace, setP2Trace] = useState<number[][][]>([])
-  // const [p2Trace, setP2Trace] = useState<string[]>([])
- 
+  const [P2Path, setP2Path] = useState<string[]>([])
+
   const handleData = async () => {
     try {
-      const raws = await FetchData(path)
+      const raws = await FetchData(URL)
       setLines(raws)
     } catch (error: any) {
       console.error("Error fetching data: ", error)
@@ -40,11 +44,10 @@ var Aoc2212 = () => {
     let seen: boolean[][] = Array.from({ length: R }, () =>
       Array(C).fill(false)
     )
-    let mp: number[][] = Array.from({ length: R }, () => 
-      Array(C).fill(0)
-    )
+    let mp: number[][] = Array.from({ length: R }, () => Array(C).fill(0))
 
     let dq: number[][] = [[sr, sc]]
+
     while (dq.length !== 0) {
       let [r, c]: number[] = dq.shift() || []
       for (let dir of D) {
@@ -66,19 +69,18 @@ var Aoc2212 = () => {
     }
     let Grid: string[] = []
     let tempGrid: string[][] = Array.from({ length: R }, () =>
-      Array(C).fill(symbol)
+      Array(C).fill(" ")
     )
     let i: number = -1
     while (++i < mp.length) {
-      // let mini: number = Math.max(...mp[i])
       let j: number = -1
       while (++j < mp[i].length) {
-        // if (mp[i][j] == mini) {
         if (mp[i][j] > 170) {
-          tempGrid[i][j] = ' '
+          tempGrid[i][j] = symbol
         }
       }
     }
+
     for (let row of tempGrid) {
       Grid.push(row.join(""))
     }
@@ -88,11 +90,12 @@ var Aoc2212 = () => {
 
   const BFS_2 = (
     arr: number[][],
+    sr: number,
+    sc: number,
     er: number,
     ec: number
   ) => {
-
-    let res:number = 1e9
+    let res: number = 1e9
     let [R, C]: number[] = [lines.length, lines[0].length]
     const D: [number, number][] = [
       [-1, 0],
@@ -101,21 +104,26 @@ var Aoc2212 = () => {
       [0, -1],
     ]
 
-    // let traceCoor:number[][][] = = Array.from({ length: R }, () => Array(C).fill([]) )
+    let parentCoordinates: number[][][] = Array.from({ length: R }, () =>
+      Array(C).fill([])
+    )
 
     let seen: boolean[][] = Array.from({ length: R }, () =>
       Array(C).fill(false)
     )
-    let mp: number[][] = Array.from({ length: R }, () => 
-      Array(C).fill(0)
-    )
+    let mp: number[][] = Array.from({ length: R }, () => Array(C).fill(0))
     let Grid: string[] = []
+    let Path: string[] = []
     let tempGrid: string[][] = Array.from({ length: R }, () =>
-      Array(C).fill('.')
+      Array(C).fill(" ")
+    )
+    let tempPath: string[][] = Array.from({ length: R }, () =>
+      Array(C).fill(".")
     )
     let dq: number[][] = [[er, ec]]
+    let [r, c]: number[] = [-1, -1]
     while (dq.length !== 0) {
-      let [r, c]: number[] = dq.shift() || []
+      [r, c] = dq.shift() || []
       for (let dir of D) {
         let rr: number = r + dir[0]
         let cc: number = c + dir[1]
@@ -124,7 +132,6 @@ var Aoc2212 = () => {
         }
         if (arr[rr][cc] === 0 && mp[rr][cc] !== 0) {
           res = Math.min(res, mp[rr][cc])
-          // tempGrid[rr][cc] = 'x'
         }
         if (seen[rr][cc]) {
           continue
@@ -136,36 +143,59 @@ var Aoc2212 = () => {
         mp[rr][cc] = mp[r][c] + 1
         dq.push([rr, cc])
 
-        // traceCoor[rr][cc] = [r, c]
-
+        parentCoordinates[rr][cc] = [r, c]
       }
     }
 
+    [r, c] = [sr, sc]
+    while (true) {
+      let [x, y] = parentCoordinates[r][c]
+      if (r === er && c === ec) {
+        break
+      }
+      tempGrid[r][c] = denseSymbol;
+      [r, c] = [x, y]
+    }
+
+    // rotated p2Grid
+    /*
+    let rotated = []
+    c = tempGrid[0].length
+    while (--c > -1) {
+      let tempRotated = []
+      r = -1
+      while (++r < tempGrid.length) {
+        tempRotated.push(tempGrid[r][c])
+      }
+      rotated.push(tempRotated)
+    }
+    tempGrid = rotated
+    */
+
+    // p2Grid
     let i: number = -1
     while (++i < mp.length) {
-      // let mini: number = Math.max(...mp[i])
       let j: number = -1
       while (++j < mp[i].length) {
-        // if (mp[i][j] == mini) {
-        if (mp[i][j] > 18) {
-          tempGrid[i][j] = ' '
+        if (mp[i][j] > density) {
+          tempPath[i][j] = ' '
         }
       }
     }
 
-    // let i: number = -1
-    // while (++i < 
-
     for (let row of tempGrid) {
       Grid.push(row.join(""))
     }
+    for (let row of tempPath) {
+      Path.push(row.join(""))
+    }
     setP2(res)
     setP2Grid(Grid)
+    setP2Path(Path)
   }
 
   const Solver = () => {
-    if (lines === undefined || lines[0] === undefined)
-      return
+    if (lines === undefined || lines[0] === undefined) return
     let arr: number[][] = []
     let [R, C]: number[] = [lines.length, lines[0].length]
     let [sr, sc, er, ec]: number[] = [-1, -1, -1, -1]
@@ -191,7 +221,7 @@ var Aoc2212 = () => {
     }
     BFS_1(arr, sr, sc, er, ec)
     // BFS_1(Deepcopy2DArray(arr), sr, sc, er, ec)
-    BFS_2(arr, er, ec)
+    BFS_2(arr, sr, sc, er, ec)
   }
 
   useEffect(() => {
@@ -205,7 +235,7 @@ var Aoc2212 = () => {
 
   return (
     <>
-      { lines ? (
+      {lines ? (
         <>
           <div className="field res-field">
             <span>--- 2022 Day 12: Hill Climbing Algorithm ---</span>
@@ -213,22 +243,36 @@ var Aoc2212 = () => {
             <span>Part 2: {p2}</span>
           </div>
           <div className="playground playground-2212">
+
             <div className="field data-field data-field-2212">
               {/* { p2Grid ? p2Grid.join('\n') : "No data available." } */}
-              { p2Grid ? p2Grid.map(line => line.split('').join(' ')).join('\n') : "No data available." }
+              { p2Grid
+                ? p2Grid.map((line) => line.split("").join(" ")).join("\n")
+                : "No data available."}
             </div>
+
+            <div className="field data-field data-field-2212">
+              {/* { p2Grid ? p2Grid.join('\n') : "No data available." } */}
+              { P2Path
+                ? P2Path.map((line) => line.split("").join(" ")).join("\n")
+                : "No data available."}
+            </div>
+
             <div className="field data-field data-field-2212">
               {/* { p1Grid ? p1Grid.join("\n") : "No data available." } */}
-              { p1Grid ? p1Grid.map(line => line.split('').join(' ')).join('\n') : "No data available." }
+              { p1Grid
+                ? p1Grid.map((line) => line.split("").join(" ")).join("\n")
+                : "No data available."}
             </div>
+
             <div className="field data-field data-field-2212">
               {lines
                 ? lines.length === 1
                   ? LenNStrsFromLine(lines[0], 16).join("\n")
-                  // : lines.join("\n")
-                  : lines.map(line => line.split('').join(' ')).join('\n')
-                // : lines.map(line => line.toUpperCase()).join('\n')
-                : "No data available."}
+                  : // : lines.join("\n")
+                    lines.map((line) => line.split("").join(" ")).join("\n")
+                : // : lines.map(line => line.toUpperCase()).join('\n')
+                  "No data available."}
             </div>
           </div>
         </>
