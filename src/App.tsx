d790f7675ef791a64c627024106e89b8d266c0d6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-23 15:04:49 +0200

    bug : JSON parsing in local storage setItem

--- src/App.tsx
+++ src/App.tsx
@@ -20,17 +20,19 @@ const routes: { [key: string]: TargetRoute } = {
   'DadJokes': DadJokes,
 }
 
+
+
 function App() {
+
+  const oldRoute = localStorage.getItem('route')
+  
   const [ route, setRoute ] = useState<string>( 
-    // '2210'
-    (window.localStorage.getItem('route'))  // save route to local storage
-    ||
+    oldRoute ? JSON.parse(oldRoute) :   // save route to local storage
     Object.keys(routes)[Math.floor(Math.random() * Object.keys(routes).length)] 
   )
 
   const handleSetRoute = (r: string) => {
     window.localStorage.setItem('route', r); // save route to local storage
-    console.log(localStorage.getItem('route'))
     setRoute(r)
   }
   const TargetRoute: TargetRoute = routes[route]
@@ -42,7 +44,8 @@ function App() {
         Hello World
       </h2>
       <div className='nav'>
-        <button className='btn'
+        <button
+          className={`btn ${ oldRoute === '1501' || route === '1501' ? 'btn-current-route' : ''}`}
           onClick={()=>handleSetRoute('1501')} >
           15:01
         </button>
@@ -50,30 +53,36 @@ function App() {
           onClick={()=>handleSetRoute('1502')} >
           15:02
         </button> */}
-        <button className='btn'
+        <button
+          className={`btn ${ oldRoute === '1802' || route === '1802' ? 'btn-current-route' : ''}`}
           onClick={()=>handleSetRoute('1802')} >
           18:02
         </button>
-        <button className='btn'
+        <button 
+          className={`btn ${ oldRoute === '1908' || route === '1908' ? 'btn-current-route' : ''}`}
           onClick={()=>handleSetRoute('1908')} >
           19:08
         </button>
-        <button className='btn'
+        <button 
+          className={`btn ${ oldRoute === '2210' || route === '2210' ? 'btn-current-route' : ''}`}
           onClick={()=>handleSetRoute('2210')} >
           22:10
         </button>
 
         {/* New */}
-        <button className='btn'
+        <button 
+          className={`btn ${ oldRoute === '2212' || route === '2212' ? 'btn-current-route' : ''}`}
           onClick={()=>handleSetRoute('2212')} >
           22:12
         </button>
 
-        <button className='btn'
+        <button 
+          className={`btn ${ oldRoute === '2223' || route === '2223' ? 'btn-current-route' : ''}`}
           onClick={()=>handleSetRoute('2223')} >
           22:23
         </button>
-        <button className='btn'
+        <button 
+          className={`btn ${ oldRoute === 'DadJokes' || route === 'DadJokes'  ? 'btn-current-route' : ''}`}
           onClick={()=>handleSetRoute('DadJokes')} >
           Jokes
         </button>
