a652a6d9465cb37ebb287445789f53341cae1056
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2023-08-23 20:52:43 +0200

    Update Aoc2212.tsx

--- src/includes/Aoc2212.tsx
+++ src/includes/Aoc2212.tsx
@@ -147,7 +147,7 @@ var Aoc2212 = () => {
       let j: number = -1
       while (++j < mp[i].length) {
         // if (mp[i][j] == mini) {
-        if (mp[i][j] > 20) {
+        if (mp[i][j] > 15) {
           tempGrid[i][j] = ' '
         }
       }
