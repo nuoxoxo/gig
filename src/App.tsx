b7cd7e5ab5f326680e6bca266f09512d2546ce7d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-24 11:38:42 +0200

    App : condensed (great!)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react"
 import Aoc2223 from "./includes/Aoc2223"
 import Aoc2210 from "./includes/Aoc2210"
 import Aoc1802 from "./includes/Aoc1802"
@@ -7,46 +7,60 @@ import Aoc1608 from "./includes/Aoc1608"
 import Aoc1908 from "./includes/Aoc1908"
 import Aoc2212 from "./includes/Aoc2212" // New
 import DadJokes from "./includes/DadJokes"
-import './styles/App.scss'
+import "./styles/App.scss"
 
 type TargetRoute = React.FC
 
 const routes: { [key: string]: TargetRoute } = {
-  '2223': Aoc2223,
-  '2212': Aoc2212, // New
-  '2210': Aoc2210,
-  '1802': Aoc1802,
-  '1501': Aoc1501,
-  '1608': Aoc1608,
-  '1908': Aoc1908,
-  'DadJokes': DadJokes,
+  "2223": Aoc2223,
+  "2212": Aoc2212, // New
+  "2210": Aoc2210,
+  "1802": Aoc1802,
+  "1501": Aoc1501,
+  "1608": Aoc1608,
+  "1908": Aoc1908,
+  DadJokes: DadJokes,
 }
 
-
-
 function App() {
+  const oldRoute = localStorage.getItem("route")
 
-  const oldRoute = localStorage.getItem('route')
-  
-  const [ route, setRoute ] = useState<string>( 
-    oldRoute ? JSON.parse(oldRoute) :   // save route to local storage
-    Object.keys(routes)[Math.floor(Math.random() * Object.keys(routes).length)] 
+  const [route, setRoute] = useState<string>(
+    oldRoute
+      ? JSON.parse(oldRoute) // save route to local storage
+      : Object.keys(routes)[
+          Math.floor(Math.random() * Object.keys(routes).length)
+        ]
   )
 
   const handleSetRoute = (r: string) => {
-    localStorage.setItem('route', JSON.stringify(r)); // save route to local storage
+    localStorage.setItem("route", JSON.stringify(r)) // save route to local storage
     setRoute(r)
   }
   const TargetRoute: TargetRoute = routes[route]
 
   return (
     <>
-      <h2 className='gig-header'
-      >
-        Hello World
-      </h2>
-      <div className='nav'>
+      <h2 className="gig-header">Hello World</h2>
+      <div className="nav">
+        {/* New code */}
 
+        {Object.keys(routes).map((key) => (
+          <button
+            key={key}
+            onClick={() => handleSetRoute(key)}
+            className={`btn ${oldRoute === key || route === key ? 'btn-current-route' : ''}`}
+          >
+            {key === 'DadJokes' ? 'Jokes' : key.replace(
+              /^(\d{2})(\d{2})$/, 
+              '$1:$2'
+            )}
+          </button>
+        ))}
+
+        {/* O.d code */}
+
+        {/*
         <button
           className={`btn ${ oldRoute === '1501' || route === '1501' ? 'btn-current-route' : ''}`}
           onClick={()=>handleSetRoute('1501')} >
@@ -109,17 +123,20 @@ function App() {
           Jokes
         </button>
 
-        <button className='btn btn-io'
-          onClick={ (e) => {
-            e.preventDefault();
-            location.href='http://nuoxoxo.github.io';
-          }}>
-          &#8203;
-          {/* &nbsp;io&nbsp; */}
-        </button>
+        */}
 
+        <button
+          className="btn btn-io"
+          onClick={(e) => {
+            e.preventDefault()
+            location.href = "http://nuoxoxo.github.io"
+          }}
+        >
+          &#8203
+          {/* &nbspio&nbsp */}
+        </button>
       </div>
-      { routes.hasOwnProperty(route) ? <TargetRoute /> : null }
+      {routes.hasOwnProperty(route) ? <TargetRoute /> : null}
     </>
   )
 }
