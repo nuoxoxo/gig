5276068d5c483f4eb1218bbee0a900d36a460c66
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2023-08-24 22:34:29 +0200

    Update Aoc2210.tsx

--- src/includes/Aoc2210.tsx
+++ src/includes/Aoc2210.tsx
@@ -106,14 +106,14 @@ var Aoc2210 = () => {
     <>
       { lines ? (
         <div className="playground">
-          <div className="field res-field res-field-2210-image"
+          <div className="field res-field res-field-1908-image"
           >
             <span>--- 2022 Day 10: Cathode-Ray Tube ---</span>
             <span>Part 1: {p1}</span>
             <span>Part 2: </span>
             <span>{ p2 ? [...p2].reverse().join('\n') : 'No data available.' }</span>
           </div>
-          <div className="field data-field data-field-2210" >
+          <div className="field data-field data-field-1908" >
             { lines
               ? lines.length === 1
                 ? LenNStrsFromLine(lines[0], 16).join("\n")
