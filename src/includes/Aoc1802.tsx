b04a127224b7735b35bafd747c188c827bf54607
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2023-08-25 10:20:14 +0200

    Update Aoc1802.tsx

--- src/includes/Aoc1802.tsx
+++ src/includes/Aoc1802.tsx
@@ -3,8 +3,8 @@ import { FetchData, LenNStrsFromLine } from "../helpers/Helpers"
 
 const URL:string = "https://raw.githubusercontent.com/nuoxoxo/in/main/1802.in"
 
-const special2 = '▓'//"🔵"
-const special3 = '░'//"🟡"
+const special3 = '▓'//"🔵"
+const special2 = '░'//"🟡"
 
 var Aoc1802 = () => {
   const [lines, setLines] = useState<string[]>([])
