2e220fd002d5a43ee2597fb19894642d977012f2
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-25 09:27:50 +0200

    2105 : init

---
+++ src/includes/Aoc2105.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react"
+import { FetchData, LenNStrsFromLine, /*Deepcopy2DArray*/ } from "../helpers/Helpers"
+
+const URL:string = 
+  "https://raw.githubusercontent.com/nuoxoxo/in/main/2105.in"
+
+var Aoc2105 = () => {
+
+  const [lines, setLines] = useState<string[]>([])
+
+  const handleData = async () => {
+    try {
+      const raws = await FetchData(URL)
+      setLines(raws)
+    } catch (error: any) {
+      console.error("Error fetching data: ", error)
+    }
+  }
+
+
+  useEffect(() => {
+    handleData()
+  }, [])
+
+  return (
+    <>
+      { lines ? (
+        <div className="playground">
+          <div className="field res-field"
+          >
+            <span>--- 2021 Day 5: Hydrothermal Venture ---</span>
+            <span>Part 1: (empty) </span>
+            <span>Part 2: (empty) </span>
+          </div>
+          <div className="field data-field data-field-2021" >
+            { lines
+              ? lines.length === 1
+                ? LenNStrsFromLine(lines[0], 16).join("\n")
+                : lines.join('\n')
+              : "No data available."}
+          </div>
+        </div>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </>
+  )
+}
+
+export default Aoc2105
