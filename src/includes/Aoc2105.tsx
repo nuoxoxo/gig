b1de60d01edad924e1fdca070a6db096d3f7ba0b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-30 12:01:27 +0200

    2105 : .match return entire string for its [0] match

--- src/includes/Aoc2105.tsx
+++ src/includes/Aoc2105.tsx
@@ -1,19 +1,39 @@
 import { useState, useEffect } from "react"
-import { FetchData, LenNStrsFromLine, /*Deepcopy2DArray*/ } from "../helpers/Helpers"
+import { FetchData, /*LenNStrsFromLine,*/ /*Deepcopy2DArray*/ } from "../helpers/Helpers"
 
 const URL:string = 
   "https://raw.githubusercontent.com/nuoxoxo/in/main/2105.in"
 
 var Aoc2105 = () => {
 
+
   const [lines, setLines] = useState<string[]>([])
+  const [parsedLines, setParsedLines] = useState<number[][]>([])
   const [p1/*, setP1*/] = useState<number>(0)
   const [p2/*, setP1*/] = useState<number>(0)
-
 
   const handleData = async () => {
     try {
       const raws = await FetchData(URL)
+      let parsedTemp:number[][] = []
+      for (let line of raws) {
+
+        let match = line.match(/(\d+),(\d+) -> (\d+),(\d+)/)
+        // console.log(match?.length, match)
+        
+        // console.log(match?.length, match)
+
+        if (match) {
+          match.shift() // pop the 1st element returned by .match() which is the string itself
+          let temp: number[] = []
+          for (let n of match) {
+            temp.push(parseInt(n)) 
+          }
+          parsedTemp.push(temp)
+          // console.log(parsedTemp.length)
+        }
+      }
+      setParsedLines(parsedTemp)
       setLines(raws)
     } catch (error: any) {
       console.error("Error fetching data: ", error)
@@ -34,12 +54,13 @@ var Aoc2105 = () => {
           >
             <span>--- 2021 Day 5: Hydrothermal Venture ---</span>
             <span>Part 1: {p1 ? p1 : '(empty)'} </span>
+
+            <div>{parsedLines?parsedLines[0].join('; '):"No data available."}</div>
+
             <span>Part 2: {p2 ? p2 : '(empty)'} </span>
             <div className="field data-field data-field-2021" > {/* Using 2021, subject to mod after */}
               { lines
-                ? lines.length === 1
-                  ? LenNStrsFromLine(lines[0], 16).join("\n")
-                  : lines.join('\n')
+                ? lines.join('\n')
                 : "No data available."
               }
             </div>
