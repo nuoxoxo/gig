a165c5383bf954ca6ec24859bf201e40370a2f43
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-20 00:18:04 +0200

    2003 : + alt infile

--- src/includes/Aoc2003.tsx
+++ src/includes/Aoc2003.tsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react"
 import { FetchData, LenNStrsFromLine, /*Deepcopy2DArray,*/ } from "../helpers/Helpers"
 
-const URL: string = "https://raw.githubusercontent.com/nuoxoxo/in/main/aoc/2003.in"
+const suffixes = ['in', 'alt']
+const choice = suffixes[Math.floor(Math.random() * suffixes.length)]
+const URL: string = "https://raw.githubusercontent.com/nuoxoxo/in/main/aoc/2003." + choice
+
 const symbolArr =  ['█', '⬤', '🔵', '🟡', '🟢', '🌕', 
   '🎾', '⭕', '🈵', '🈶', '🍊', '🦁', '🐯']
 const symbol = symbolArr[Math.floor(Math.random() * symbolArr.length)]
