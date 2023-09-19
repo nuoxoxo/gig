c0ff2c68edd13c452c52d29411eb6022055f0cda
uxn <malcolm.nuo.xu@gmail.com>
2023-09-19 12:16:06 +0200

    1810 : done

--- src/includes/Aoc1810.tsx
+++ src/includes/Aoc1810.tsx
@@ -5,11 +5,12 @@ import {
   // Deepcopy2DArray,
 } from "../helpers/Helpers"
 
-const URL: string = 
+const URL: string =
   "https://raw.githubusercontent.com/nuoxoxo/in/main/aoc/1810.0"
 
 var Aoc1810 = () => {
   const [lines, setLines] = useState<string[]>([])
+  const [images, setImages] = useState<string[][]>([])
 
   const handleData = async () => {
 
@@ -21,9 +22,63 @@ var Aoc1810 = () => {
     }
   }
 
+  const Solver = () => {
+    let res:string[][] = []
+    let A: number[][] = []
+    for (let line of lines) {
+      let [x, y, dx, dy] = line.match(/-?\d+/g)!.map(Number)
+      A.push([x, y, dx, dy])
+    }
+
+    console.log(A)
+
+    let i = -1
+    while (++i < 10600) { // range is hand picked
+      let hix: number = Math.max(...A.map(([n, , ,]) => n))
+      let lox: number = Math.min(...A.map(([n, , ,]) => n))
+      let hiy: number = Math.max(...A.map(([, n, ,]) => n))
+      let loy: number = Math.min(...A.map(([, n, ,]) => n))
+      let offset = 100
+
+      if (lox + offset > hix && loy + offset > hiy) {
+        let temp:string[] = [' --- ' + i.toString() + ' --- ']
+        // console.log(i)
+
+        let r = loy - 1
+        // let s = '' // Bug 
+        while (++r < hiy + 1) {
+          let c = lox - 1
+          let s = ''
+          while (++c < hix + 1) {
+            const temp = A.map(([x, y, ,]) => [x, y])
+            if (temp.some(([x, y]) => x === c && y === r)) {
+              s += '#'
+            } else {
+              s += '.'
+            }
+          }
+          // s += '\n'
+          temp.push(s)
+        }
+        // console.log(s)
+        res.push(temp)
+      }
+      let j = -1
+      while (++j < A.length) {
+        A[j][0] += A[j][2]
+        A[j][1] += A[j][3]
+      }
+    }
+    setImages(res)
+  }
+
   useEffect(() => {
     handleData()
   }, [])
+
+  useEffect(() => {
+    Solver()
+  }, [lines])
 
   return (
     <>
@@ -31,15 +86,18 @@ var Aoc1810 = () => {
         <>
 
           <div className="playground">
-            <div className="field res-field">
+            <div className="field res-field res-field-1810">
               <span>--- 2018 Day 10: The Stars Align ---</span>
               {/* <span>Part 1: {p1 ? p1 : "(empty)"} </span> */}
               {/* <span>Part 2: {p2 ? p2 : "(empty)"} </span> */}
+              <div className='res-field-1810-image'>
+                {images ? images.map(line => line.join('\n')).join('\n') : "No data available."}
+              </div>
             </div>
           </div>
 
           <div className="field data-field data-field-2021">
-            { lines ? lines.join("\n") : "No data available." }
+            {lines ? lines.join("\n") : "No data available."}
           </div>
 
         </>
